2dc3d8625566a2b7ec36a77018265d168e928435
ephraim <ephraims.818@gmail.com>
2020-05-08 03:37:20 +0600

    commit all sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,5 +48,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var deadline = new Date(Date.parse(new Date()) + 60 * 24 * 60 * 60 * 1000);
 initializeClock('clockdiv', deadline);
