fe6025b7872462fd3864b2ac6503be52454d2752
ephraim <ephraims.818@gmail.com>
2020-05-08 22:21:18 +0600

    correction -1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-var count = new Date('july 8, 2020 00:00:00').getTime();
+var count = new Date('july 7, 2020 00:00:00').getTime();
 var x = setInterval(function () {
   var now = new Date().getTime();
   var d = count - now;
